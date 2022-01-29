01e982bbfc803a963896f8ca3d4204772393d84d
Alexander Rovang <DXRovang@gmail.com>
2022-01-28 23:55:31 -0500

    sets up useState & useEffect

--- src/FishList.js
+++ src/FishList.js
@@ -1,6 +1,26 @@
+import { useState, useEffect } from "react";
+import Fish from './Fish'
+
 const FishList = () => {
+
+  const [fish, setFish] = useState([])
+
+  useEffect(()=>{
+    fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
+    .then(r=>r.json())
+    .then(data=>{
+      setFish(data)
+      console.log(data)
+    })
+    .catch((err) => console.log(err))
+  }, [])
+
+
   return ( 
-    <div>In FishList</div>
+    <div>
+      In FishList
+      {fish && <Fish fish={fish}/>}
+      </div>
    );
 }
  
